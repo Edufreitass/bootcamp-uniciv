075371a9efa95a451d99d020cee1db2794f941cc
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-10-25 16:45:50 -0300

    Criação do projeto Spring Boot

---
+++ cursos-backend/src/main/java/br/com/cursos/CursosBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosBackendApplication.class, args);
+	}
+
+}
